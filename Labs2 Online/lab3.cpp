ab798ab8c3ca416f9854c37eebf1981e719fa097
mifta <miftarosky@hotmail.com>
2024-03-19 02:37:05 +0100

    😎 adding files 😎

---
+++ Labs2 Online/lab3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class SuperString {
+private:
+public:
+};
+int main() {
+
+}
--- vezba2.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout<<"Koj si be ti"<<endl;
-}
